8cb85dbf7e3d4f7ac76ebddc9abd27e0dedc715b
PhuocHN <huuphuocit04@gmail.com>
2025-06-18 16:57:10 +0700

    feat: add call api function

---
+++ src/app/service/ChatListService.jsx
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8000/api/";
+
+export const fetchChatList = async (user_id) => {
+  try {
+    const response = await axios.get(`${API_URL}room/user/list_chat/${user_id}`,  );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching chat list:", error);
+    throw error;
+  }
+};

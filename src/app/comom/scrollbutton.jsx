f62febc5d0939ae36c0e8c0428de69f102f3a517
PhuocHN <huuphuocit04@gmail.com>
2025-06-19 10:07:48 +0700

    feat: scroll dow common

---
+++ src/app/comom/scrollbutton.jsx
@@ -0,0 +1,42 @@
+'use client'; // Nếu dùng app router
+import { HiArrowDownCircle } from "react-icons/hi2";
+import { useEffect, useState } from 'react';
+
+export default function ScrollToBottomButton({ scrollRef }) {
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    const scrollContainer = scrollRef?.current;
+    if (!scrollContainer) return;
+
+    const handleScroll = () => {
+      const bottom =
+        Math.ceil(scrollContainer.scrollTop + scrollContainer.clientHeight) >= scrollContainer.scrollHeight - 2;
+      setShowButton(!bottom);
+    };
+
+    scrollContainer.addEventListener('scroll', handleScroll);
+    // Kiểm tra trạng thái ban đầu
+    handleScroll();
+    return () => scrollContainer.removeEventListener('scroll', handleScroll);
+  }, [scrollRef]);
+
+  const scrollToBottom = () => {
+    const scrollContainer = scrollRef?.current;
+    if (scrollContainer) {
+      scrollContainer.scrollTo({ top: scrollContainer.scrollHeight, behavior: 'smooth' });
+    }
+  };
+
+  return (
+    showButton && (
+      <button
+        onClick={scrollToBottom}
+        className="fixed bottom-15 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[500] bg-gray-300 hover:bg-blue-200 text-white p-4 rounded-full shadow-lg transition duration-300 "
+        aria-label="Scroll to bottom"
+      >
+        <HiArrowDownCircle />
+      </button>
+    )
+  );
+}

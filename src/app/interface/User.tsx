779649b54a588ab35c27a3782d5fdb6794a1d9c0
PhuocHN <huuphuocit04@gmail.com>
2025-06-23 13:40:41 +0700

    add: user interface

---
+++ src/app/interface/User.tsx
@@ -0,0 +1,9 @@
+export interface User {
+    user_id: string;
+    username: string;
+    email: string;
+    img_url: string;
+    created_at: string;
+    display_name: string;
+    flagDelete: boolean;
+}

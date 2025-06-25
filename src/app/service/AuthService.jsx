133c231ef3b66bc14d46adc253a8f042a1485a82
PhuocHN <huuphuocit04@gmail.com>
2025-06-25 10:59:25 +0700

    modify: clear api

--- src/app/service/AuthService.jsx
+++ src/app/service/AuthService.jsx
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api/auth';
+const API_URL = 'http://localhost:8000/api';
 
 export const login = async (user) => {
   try {
-    const response = await axios.post(`${API_URL}/login`, user);
+    const response = await axios.post(`${API_URL}/auth/login`, user);
     return response.data;
   } catch (error) {
     console.error('Login error:', error);
@@ -14,7 +14,7 @@ export const login = async (user) => {
 
 export const register = async (user) => {
   try {
-    const response = await axios.post(`${API_URL}/register`, user);
+    const response = await axios.post(`${API_URL}/auth/register`, user);
     return response.data;
   } catch (error) {
     console.error('Registration error:', error);
--- src/app/service/ChatListService.jsx
+++ src/app/service/ChatListService.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api/";
+const API_URL = "http://localhost:8000/api";
 
 export const fetchChatList = async (user_id) => {
   try {
-    const response = await axios.get(`${API_URL}room/user/list_chat/${user_id}`,  );
+    const response = await axios.get(`${API_URL}/room/user/list_chat/${user_id}`,  );
     return response.data;
   } catch (error) {
     console.error("Error fetching chat list:", error);

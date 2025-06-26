45be939ae22d7a607b1851fdb8fb6ad155cf171a
PhuocHN <huuphuocit04@gmail.com>
2025-06-26 09:21:08 +0700

    modify: api url

--- src/app/service/ChatListService.jsx
+++ src/app/service/ChatListService.jsx
@@ -4,7 +4,7 @@ const API_URL = "http://localhost:8000/api";
 
 export const fetchChatList = async (user_id) => {
   try {
-    const response = await axios.get(`${API_URL}/room/user/list_chat/${user_id}`,  );
+    const response = await axios.get(`${API_URL}/room/user/list_chat/${user_id}`);
     return response.data;
   } catch (error) {
     console.error("Error fetching chat list:", error);

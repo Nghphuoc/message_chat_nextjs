a9300bece20adfc57ff604a374e3d8c82c9c0996
PhuocHN <huuphuocit04@gmail.com>
2025-07-18 10:27:30 +0700

    feat: reply and edit delete message dropdown

--- src/app/comom/showmessage.jsx
+++ src/app/comom/showmessage.jsx
@@ -5,7 +5,7 @@ import "../../css/hiddenscroll.css";
 import 'dayjs/locale/vi';
 import { format, isToday, isYesterday, isSameDay } from 'date-fns';
 import { vi } from 'date-fns/locale';
-import { deleteIcon } from '@/app/service/MessageService'
+import { Toaster, toast } from "react-hot-toast";
 
 const EMOJIS = ["😁", "❤️", "😍", "👍", "😭", "😡"];
 
@@ -53,14 +53,16 @@ const ShowMessage = ({
         return format(date, 'EEEE, dd/MM/yyyy', { locale: vi });
     };
 
-    // ====== API: Xóa reaction (không dùng trực tiếp ở đây) ======
-    const fetchingDeletereaction = async (reaction_id, user_id) => {
-        try {
-            const response = await deleteIcon(reaction_id, user_id);
-            console.log(response);
-        } catch (error) {
-            console.error("error: ", error);
-        }
+    function formatMessageText(message) {
+        const urlRegex = /(https?:\/\/[^\s]+)/g;
+        const phoneRegex = /\b(0\d{9,10}|\+84\d{9,10})\b/g;
+        const emailRegex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g;
+
+        let formatted = message
+            .replace(urlRegex, (url) => `<a href="${url}" class= "underline"target="_blank">${url}</a>`)
+            .replace(phoneRegex, (phone) => `<a href="tel:${phone}" class="underline">${phone}</a>`)
+            .replace(emailRegex, (email) => `<a href="mailto:${email}" class="underline">${email}</a>`);
+        return <span dangerouslySetInnerHTML={{ __html: formatted }} />;
     }
 
     // ====== RENDER ======
@@ -69,6 +71,7 @@ const ShowMessage = ({
             ref={scrollRef}
             className="flex-1 overflow-y-auto p-4 space-y-3 bg-white scrollbar-hide"
         >
+            <Toaster />
             {/* ====== HIỂN THỊ DANH SÁCH TIN NHẮN ====== */}
             {messages && messages.length > 0 ? (
                 messages.map((msg, index) => {
@@ -109,13 +112,19 @@ const ShowMessage = ({
                                             ${isMe
                                                 ? 'bg-gradient-to-r from-blue-500 to-purple-600 text-white rounded-2xl rounded-br-md'
                                                 : 'bg-gray-200 text-gray-900 rounded-2xl rounded-bl-md'}`} >
-                                        <p>{msg.content}</p>
+                                        {msg.reply && (
+                                            <div className={`mb-1 px-2 py-1 rounded-xl text-xs ${isMe ? 'bg-white/20 text-white' : 'bg-white text-gray-700'} border-l-4 ${isMe ? 'border-blue-300' : 'border-gray-300'}`}>
+                                                <p className="font-semibold truncate max-w-[200px]">{msg.reply.name_user}</p>
+                                                <p className="italic truncate max-w-[220px]">{msg.reply.content}</p>
+                                            </div>
+                                        )}
+                                        <p>{formatMessageText(msg.content)}</p>
                                         {/* ====== NÚT BA CHẤM (MENU) ====== */}
                                         <button
                                             onClick={() => setActiveMenu(activeMenu === msg.message_id ? null : msg.message_id)}
                                             className={`absolute -bottom-2 p-1.5 rounded-full opacity-0 group-hover:opacity-100 ${isMe
                                                 ? 'bg-white/20 hover:bg-white/30 -left-4'
-                                                : 'bg-gray-300/50 hover:bg-gray-400/50 -right-5'
+                                                : 'bg-gray-300/50 hover:bg-gray-400/50 -right-5 text-black'
                                                 }`}
                                         >
                                             <svg className="w-4 h-4 text-gray-600" fill="currentColor" viewBox="0 0 24 24">
@@ -131,24 +140,43 @@ const ShowMessage = ({
                                                 className={`absolute top-full mt-2 ${isMe ? 'right-1/8' : 'left-1/8'} bg-white border border-gray-200 rounded-2xl shadow-lg p-1 z-50 w-[150px]`}
                                             >
                                                 <button
-                                                    className="block w-full px-4 py-2 text-black hover:bg-gray-100 rounded"
+                                                    className="block w-full px-4 py-2 text-left text-black hover:bg-gray-100 rounded"
                                                     onClick={() => {
                                                         setActiveMenu(null);
                                                         setActiveEmojiPicker(msg.message_id);
+                                                        setActiveMenu(!activeMenu);
                                                     }}
                                                 >
                                                     Send Reaction
                                                 </button>
+                                                <button className="block w-full px-4 py-2 text-left text-black hover:bg-gray-100 rounded"
+                                                    onClick={() => {
+                                                        toast("chức năng đang phát triển!!!");
+                                                        setActiveMenu(!activeMenu);
+                                                    }}>
+                                                    Reply
+                                                </button>
+
                                                 {isMe && (
-                                                    <button
-                                                        className="block w-full text-left px-4 py-2 hover:bg-gray-100 rounded text-red-500"
-                                                        onClick={() => {
-                                                            setActiveMenu(null);
-                                                            deleteMessage(msg.message_id);
-                                                        }}
-                                                    >
-                                                        🗑️ Delete
-                                                    </button>
+                                                    <div>
+                                                        <button className="block w-full px-4 py-2 text-left text-black hover:bg-gray-100 rounded"
+                                                            onClick={() => {
+                                                                toast("chức năng đang phát triển!!!");
+                                                                setActiveMenu(!activeMenu);
+                                                            }}>
+                                                            Edit message
+                                                        </button>
+                                                        <button
+                                                            className="block w-full text-left px-4 py-2 hover:bg-gray-100 rounded text-red-500"
+                                                            onClick={() => {
+                                                                setActiveMenu(null);
+                                                                deleteMessage(msg.message_id);
+                                                                setActiveMenu(!activeMenu);
+                                                            }}
+                                                        >
+                                                            🗑️ Delete
+                                                        </button>
+                                                    </div>
                                                 )}
                                             </div>
                                         )}
@@ -224,23 +252,25 @@ const ShowMessage = ({
                 </div>
             )}
             {/* ====== TYPING INDICATOR ====== */}
-            {isTyping && (
-                <div className="flex space-x-2">
-                    <div className="z-50 w-8 h-8 bg-gradient-to-b from-violet-700/80 to-blue-900/80 rounded-full flex items-center justify-center">
-                        <svg className="w-3 h-3 text-white" fill="currentColor" viewBox="0 0 24 24">
-                            <path d="M20 2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h4l4 4 4-4h4c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2z" />
-                        </svg>
-                    </div>
-                    <div className="bg-gray-100 rounded-2xl rounded-tl-md px-3 py-2">
-                        <div className="flex space-x-1">
-                            <div className="w-2 h-2 bg-gray-400 rounded-full animate-bounce"></div>
-                            <div className="w-2 h-2 bg-gray-400 rounded-full animate-bounce" style={{ animationDelay: '0.1s' }}></div>
-                            <div className="w-2 h-2 bg-gray-400 rounded-full animate-bounce" style={{ animationDelay: '0.2s' }}></div>
+            {
+                isTyping && (
+                    <div className="flex space-x-2">
+                        <div className="z-50 w-8 h-8 bg-gradient-to-b from-violet-700/80 to-blue-900/80 rounded-full flex items-center justify-center">
+                            <svg className="w-3 h-3 text-white" fill="currentColor" viewBox="0 0 24 24">
+                                <path d="M20 2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h4l4 4 4-4h4c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2z" />
+                            </svg>
+                        </div>
+                        <div className="bg-gray-100 rounded-2xl rounded-tl-md px-3 py-2">
+                            <div className="flex space-x-1">
+                                <div className="w-2 h-2 bg-gray-400 rounded-full animate-bounce"></div>
+                                <div className="w-2 h-2 bg-gray-400 rounded-full animate-bounce" style={{ animationDelay: '0.1s' }}></div>
+                                <div className="w-2 h-2 bg-gray-400 rounded-full animate-bounce" style={{ animationDelay: '0.2s' }}></div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            )}
-        </div>
+                )
+            }
+        </div >
     );
 };
 
